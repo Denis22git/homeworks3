b41348d245e03b89c2045187492456dc05302be0
«Denis» <denis.ch.9797@mail.ru>
2022-07-01 00:34:58 +0500

    added homeworks and gitignore

---
+++ homework01/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите 5ти значное число: ");
+string number = Console.ReadLine();
+    bool A = true;
+    for (int i = 0; i < (number.Length - 1) / 2; i++)
+    {
+        if (number[i] != number[number.Length - i - 1])
+        {
+            A = false;
+        }
+    }        
+    if (A)
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else 
+    {
+        Console.WriteLine("Не палиндром");
+    }
+    Console.ReadKey();
